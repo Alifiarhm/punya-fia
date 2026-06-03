1766eca1e9d5a205c4c850e6d6cb751b0e9e1c55
Alifiarhm <alifiarahma2006@gmail.com>
2026-06-03 08:41:22 +0700

    Add files via upload

---
+++ TUGAS DAN LATIHAN/latihan2_m9.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class latihan2_m9 {
+    public static void main(String[] args) {
+        /*
+        Buatlah fungsi isGanjil() yang menerima satu parameter bertipe int. 
+        Fungsi mengembalikan nilai boolean: true jika bilangan ganjil, false jika genap. Uji dengan beberapa angka.
+        Hint: gunakan operator % (modulus)
+
+        Clue : 
+        - tipe fungsi int
+        - retur value : boleean
+         */
+    Scanner sc = new Scanner (System.in);
+    int a=0;
+    System.out.print("Masukkan Angka = ");
+    a = sc.nextInt();
+
+    if (isGanjil (a)) {
+        System.out.println("Genap");
+    } else {
+        System.out.println("Ganjil");
+    }
+    }
+
+    static boolean isGanjil (int angka) {
+        if (angka % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+     
+    }
+    
+}
